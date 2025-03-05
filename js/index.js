13789e0b4ddc1f5fcaf4e3be96297dee18861a45
pety02 <pety02@abv.bg>
2025-03-05 11:01:42 +0200

    second task implemented - autocomplete

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 let input = document.getElementById("input");
+let btn = document.getElementById("todos-btn");
 
-function getTODOS () {
+btn.addEventListener("click", function  () {
     const todoLi = document.createElement("li");
     todoLi.textContent = input.value;
     input.value = "";
     document.getElementById("todos-list").appendChild(todoLi);
-}
+});
+
+
+const input2 = document.getElementById("input-countries");
+const countries = ["Bulgaria", "England", "USA", "Italy", "Norway"];
+input2.addEventListener("keyup", function() {
+    if(input2.value.length >= 3) {
+        const match = countries.find(c => c.toLowerCase().includes(input2.value.toLowerCase()));
+        if (match) {
+            input2.value = match; 
+        }
+    }
+});
